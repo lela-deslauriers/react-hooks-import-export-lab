d55cf02217bad9c7bba52bc16f86ab62f7b08ba3
Lela Deslauriers <lela.deslauriers@gmail.com>
2023-02-19 22:12:05 -0700

    commit tree 1f2c3c289d7d00cd09e511c676118c3f8aac685e

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Home from "../components/Home";
+import NavBar from "../components/NavBar";
+import About from "../components/About"
+import {username, city, image} from "../data/user"
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city}/>
+      <About image={image}/>
     </div>
   );
 }
